9c2f85e678836d5a12e3aa4bec108d89c075236f
dansh <smartlinux@ya.ru>
2022-06-18 13:46:27 +0200

    sunxi-current - stable linux, sunxi-edge - mainline linux

--- lib/compilation-prepare.sh
+++ lib/compilation-prepare.sh
@@ -123,11 +123,10 @@ compilation_prepare()
 	if linux-version compare "${version}" ge 5.10 && [ $SKIP_BOOTSPLASH != yes ]; then
 
 		display_alert "Adding" "Kernel splash file" "info"
-		if linux-version compare "${version}" ge 5.11; then
+		if linux-version compare "${version}" lt 5.19; then
 			process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0000-Revert-fbcon-Avoid-cap-set-but-not-used-warning.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0001-Revert-fbcon-Add-option-to-enable-legacy-hardware-ac.patch" "applying"
 		fi
-
-		process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0001-Revert-fbcon-Add-option-to-enable-legacy-hardware-ac.patch" "applying"
 
 		if linux-version compare "${version}" ge 5.15; then
 			process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0002-Revert-vgacon-drop-unused-vga_init_done.patch" "applying"
@@ -137,20 +136,25 @@ compilation_prepare()
 		process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0004-Revert-drivers-video-fbcon-fix-NULL-dereference-in-f.patch" "applying"
 		process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0005-Revert-fbcon-remove-no-op-fbcon_set_origin.patch" "applying"
 		process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0006-Revert-fbcon-remove-now-unusued-softback_lines-curso.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0007-Revert-fbcon-remove-soft-scrollback-code.patch" "applying"
 
-		process_patch_file "${SRC}/patch/misc/0001-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0002-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0003-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0004-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0005-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0006-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0007-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0008-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0009-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0010-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0011-bootsplash.patch" "applying"
-		process_patch_file "${SRC}/patch/misc/0012-bootsplash.patch" "applying"
+		if linux-version compare "${version}" lt 5.19; then
+			process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0007-Revert-fbcon-remove-soft-scrollback-code.patch" "applying"
+		fi
+
+		if linux-version compare "${version}" lt 5.19; then
+			process_patch_file "${SRC}/patch/misc/0001-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0002-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0003-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0004-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0005-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0006-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0007-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0008-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0009-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0010-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0011-bootsplash.patch" "applying"
+			process_patch_file "${SRC}/patch/misc/0012-bootsplash.patch" "applying"
+		fi
 
 	fi
 
@@ -398,7 +402,7 @@ compilation_prepare()
 
 
 	# Wireless drivers for Xradio XR819 chipsets
-	if linux-version compare "${version}" ge 4.19 && [[ "$LINUXFAMILY" == sunxi* ]] && [[ "$EXTRAWIFI" == yes ]]; then
+	if linux-version compare "${version}" lt 5.19 && [[ "$LINUXFAMILY" == sunxi* ]] && [[ "$EXTRAWIFI" == yes ]]; then
 
 		display_alert "Adding" "Wireless drivers for Xradio XR819 chipsets" "info"
 
